0e8e951324d8e51bab2495e2c90348fafdfc76a4
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-10-23 15:02:28 +0800

    aa

--- accountmgr.py
+++ accountmgr.py
@@ -28,13 +28,106 @@ class accountMgr(object):
     def threadFun(self,arg):
 
        while(True):
+
+           if(!start_loop):
             
-            #voters = self.requestVoters(self.sartAccount)
+                min_account_id = get_min_account_id();
+                max_account_id = get_max_account_id();
+                cur_account_id = min_account_id;
+                start_loop = true;
+           
+            if(cur_account_id >= 0):
+               if(self.monitoraccount(cur_account_id)):
+
+                  cur_account_id = cur_account_id + 1;
+                  if(cur_account_id > max_account_id):
+                     start_loop = false; 
+
             time.sleep(1)
-            print("ttt")
+    
+    def monitoraccount(self,accountid):
+
+        flag = False
+
+        account = get_account_name(accountid)
+        if(account == ""):
+            return True
+	
+        flag = update_token(account)
+        flag = update_stake(account)
+
+        return flag;                      
+
+    def update_token(self):
+       print("update_token"
+
+    def udpate_stake(self):
+       print("update_stake")
 
     def Start(self):
-            
+         
+         start_loop = False
+         min_account_id = 0
+         max_account_id = 0
+	 cur_account_id = 0
+ 
          t =threading.Thread(target=self.threadFun,args=(1,))
          t.setDaemon(True)#设置线程为后台线程
          t.start()
+
+    def get_min_account_id(self):
+
+        min = -1
+
+	try:
+
+	    db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
+            cursor = db.cursor()
+
+            sql = "select min(id) from accounts"
+	    cursor.execute(sql)
+            
+            for row in cursor.fetchall():
+                min = row[0]
+
+         except:
+            print("get_min_account_id error")
+         
+         return min
+
+    def get_max_account_id(self):
+
+        max = -1
+
+        try:
+
+            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
+            cursor = db.cursor()
+
+            sql = "select max(id) from accounts"
+            cursor.execute(sql)
+
+            for row in cursor.fetchall():
+                max = row[0]
+
+         except:
+            print("get_max_account_id error")
+         
+         return max
+
+    def get_account_name(self,accoutid):
+         
+       account = ""
+       try:
+            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
+            cursor = db.cursor()
+
+            sql = "select name from accounts where id=%d" %(accountid)
+            cursor.execute(sql)
+            
+            for row in cursor.fetchall():
+                account = row[0]
+       except:
+            print("get_account_name error")
+
+       return account                          
