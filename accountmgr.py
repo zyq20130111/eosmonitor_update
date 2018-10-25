ad232282e35597e9a9a775df622cc4a8c2ed2cc0
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-10-25 17:02:09 +0800

    aa

--- accountmgr.py
+++ accountmgr.py
@@ -157,7 +157,7 @@ class accountMgr(object):
             db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
             cursor = db.cursor()
             print "tttt"
-	    ttt = "INSERT INTO tokens (account,symbol ,balance,symbol_precision,contract_owner)"
+	    ttt = "INSERT INTO tokens (account,symbol ,balance,symbol_precision,contract_owner) VALUES('%s','%s' ,'%s' , %d ,'%s')" %(account,symbol,quantity,precision,contract)
             print ttt
             sql = "INSERT INTO tokens (account,symbol ,balance,symbol_precision,contract_owner)  VALUES('%s','%s' ,'%s' , %d ,'%s') ON DUPLICATE KEY UPDATE balance='%s',symbol_precision=%d" %(account,symbol,quantity,precision,contract,quantity,symbol_precision)
             
